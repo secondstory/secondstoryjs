84fffb682079c173b6d19ff30a3a7869e5ee28ff
Thomas Reynolds <tdreyno@gmail.com>
2010-08-31 10:21:34 -0700

    deps

--- test/qunit/qunit.js
+++ test/qunit/qunit.js
@@ -3,4 +3,6 @@ steal
   .then("../../controller/state_machine/test/qunit/qunit")
   .then("../../controller/map_states_to_class/test/qunit/qunit")
   .then("../../controller/autoloading/test/qunit/qunit")
-  .then("../../router/test/qunit/qunit")
+  .then("../../router/controller/test/qunit/qunit")
+  .then("../../router/sherpa/test/qunit/qunit")
+  .then("../../router/subscribe/test/qunit/qunit")
