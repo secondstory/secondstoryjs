abdc250a87790b5dccc10b55d3857adbbe627d49
Thomas Reynolds <tdreyno@gmail.com>
2010-09-01 17:38:29 -0700

    move html5store

--- test/qunit/qunit.js
+++ test/qunit/qunit.js
@@ -1,5 +1,5 @@
 steal
-  .then("../../model/local_store/test/qunit/qunit")
+  .then("../../model/html5_store/local/test/qunit/qunit")
   .then("../../controller/state_machine/test/qunit/qunit")
   .then("../../controller/map_states_to_class/test/qunit/qunit")
   .then("../../controller/autoloading/test/qunit/qunit")
